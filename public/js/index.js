dac7ef83d8989f1713e0cc047b218b790f3b9df9
VaCool <18rammzes@gmail.com>
2018-07-06 21:39:27 +0300

    created a modal window for authentication
    created a modal window for authentication

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 
-
+$('#workspace').load('./news.html');
 $(document).ready(function() {
 
 	var li0 = $('#li0');
@@ -10,6 +10,11 @@ $(document).ready(function() {
  	var li5 = $('#li5');
  	var li6 = $('#li6');
  	var li7 = $('#li7');
+ 	var buttonLogin = $('#buttonLogin, #login');
+	var buttonSingUp = $('#buttonSingUp, #signUp');
+	var saveSignUp = $('#saveSignUp');
+	var saveLogin = $('#saveLogin');
+	var buttonExit = $('#buttonExit');
 
 	li0.click(function(event){ 
 		pageLoading("li0");
@@ -43,6 +48,28 @@ $(document).ready(function() {
 		pageLoading("li7");
 	});
 
+	buttonLogin.click( function(event){ 
+		$('#loginModal').modal("show");
+        $('#forsignUp').hide(); 
+        $('#forLogin').show();
+        $('#login').parent().css({"background":"#826464", "border-bottom-right-radius":"20px", "border-top-right-radius":"20px" }); 
+        $('#signUp').parent().css({"background":"#4c2c2c", "border-radius:":"none" }); 
+    });
+
+	buttonSingUp.click( function(event){ 
+        $('#loginModal').modal("show");
+        $('#forLogin').hide(); 
+        $('#forsignUp').show();
+        $('#signUp').parent().css({"background":"#826464", "border-bottom-left-radius":"20px", "border-top-left-radius":"20px"}); 
+        $('#login').parent().css({"background":"#4c2c2c", "border-radius:":"none" }); 
+
+    });
+
+
+
+
+
+
 	function pageLoading(id){
 
 		switch(id) {
@@ -50,22 +77,22 @@ $(document).ready(function() {
 				$('#workspace').load('./news.html');
     			break;
 			case 'li1':  
-				$('.workspace').load('./newss.html');
-    			break;
-			case 'li2':  
 				$('.workspace').load('./pizza.html');
     			break;
-   	 		case 'li3':  
+			case 'li2':  
 				$('.workspace').load('./paste.html');
     			break;
-    		case 'li4':  
+   	 		case 'li3':  
 				$('.workspace').load('./risotto.html');
     			break;
-    		case 'li5':  
+    		case 'li4':  
 				$('.workspace').load('./dessert.html');
     			break;
-    		case 'li6':  
+    		case 'li5':  
 				$('.workspace').load('./reviews.html');
+    			break;
+    		case 'li6':  
+				$('.workspace').load('./basket.html');
     			break;
     		case 'li7':  
 				$('.workspace').load('./basket.html');
