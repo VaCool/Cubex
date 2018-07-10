813c836bab13bd9432e9dcf1ea47c330468ccb17
VaCool <18rammzes@gmail.com>
2018-07-10 10:40:54 +0300

    created the control panel (without styles) with the functionality to add, delete, change
    created the control panel (without styles) with the functionality to add, delete, change

--- public/js/pizza.js
+++ public/js/pizza.js
@@ -1,3 +1,7 @@
+    var id, i = 0;   
+    var massOfAllId = new Array();
+    var massOfAllNames = new Array();
+    var massOfVisibleId = new Array();
 $(document).ready(function() { 
     getPizza();
     function getPizza(){
@@ -18,39 +22,46 @@ $(document).ready(function() {
                 $contentTemplate.find("[data-url-big]").attr("src", content.URL);          
                 $contentTemplate.find("[data-name-second]").text(content.name);
                 $contentTemplate.find("[data-consist]").text(content.consist);              
-                $contentWrapper.append($contentTemplate);           
+                $contentWrapper.append($contentTemplate);
+                massOfAllId[i] = "#" + "modalId" + content.pizza_id ;
+                massOfAllNames[i] = content.name;
+                i++;
+
             });
             var open_modal = $('.open_modal');
+            var open_adminModal = $('#open_adminModal');
             var previousButton = $('.previousButton');
             var nextButton = $('.nextButton');
-            var massId = new Array();
-            var id, i;   
+            var buttonDo = $('#buttonDo');
+  
+
+
 
             function getMassId(){
                 $(".forModal").each(function (i) {
                     if ($(this).parent().is(":visible")){
-                        massId.push($(this).attr('id'));
+                        massOfVisibleId.push($(this).attr('id'));
                     }
                 });
             }
             previousButton.click( function(event){ 
-                massId = [];
+                massOfVisibleId = [];
                 getMassId();
-                i = $.inArray(id, massId)
-                    i==0 ? i=massId.length-1: i--;
+                i = $.inArray(id, massOfVisibleId)
+                    i==0 ? i=massOfVisibleId.length-1: i--;
                     $('.forDelete').detach();
-                    $("#"+massId[i]).children().clone().addClass("forDelete").appendTo(".forGalley");
-                    id = massId[i];
+                    $("#"+massOfVisibleId[i]).children().clone().addClass("forDelete").appendTo(".forGalley");
+                    id = massOfVisibleId[i];
             });
 
             nextButton.click( function(event){ 
-                massId = [];
+                massOfVisibleId = [];
                 getMassId();
-                i = $.inArray(id, massId)
-                    i==massId.length-1 ? i=0 : i++;
+                i = $.inArray(id, massOfVisibleId)
+                    i==massOfVisibleId.length-1 ? i=0 : i++;
                     $('.forDelete').detach();
-                    $("#"+massId[i]).children().clone().addClass("forDelete").appendTo(".forGalley");
-                    id = massId[i];
+                    $("#"+massOfVisibleId[i]).children().clone().addClass("forDelete").appendTo(".forGalley");
+                    id = massOfVisibleId[i];
             });
 
             open_modal.click( function(event){ // лoвим клик пo ссылке с клaссoм open_modal
@@ -60,8 +71,141 @@ $(document).ready(function() {
                 $(this).parent().find('.forClone').clone().addClass("forDelete").appendTo(".forGalley");
             });
 
+            open_adminModal.click( function(event){ // лoвим клик пo ссылке с клaссoм open_modal
+                
+                $('.forDelete').detach();
+                id = ($(this).parent().find('.forModal').attr( "id"));
+                $(this).parent().find('.forClone').clone().addClass("forDelete").appendTo(".forGalley");
+                $("#contentTitle").empty();
 
-   
+                $('#adminModal').modal("show");
+                for(i = 0; i < massOfAllId.length; i++){
+                    $("#contentTitle").prepend( $('<option value="' + massOfAllId[i] + '">' + massOfAllNames[i] 
+                    + '</option>'));
+                }
+            });
+
+            $('#addContent').click( function(event){ // лoвим клик пo ссылке с клaссoм open_modal
+                $("#contentTitle").attr("disabled",true);
+                alert("hi");
+            });
+
+             function changeSelect(){
+                $("#adminModal").find("input[name ='name']").val($($("#contentTitle :selected")
+                    .val()).parent().find("[data-name-first]").text());
+                $("#adminModal").find("input[name ='weight']").val($($("#contentTitle :selected")
+                    .val()).parent().find("[data-weight]").text());
+                $("#adminModal").find("input[name ='price']").val($($("#contentTitle :selected")
+                    .val()).parent().find("[data-price]").text());
+                $("#adminModal").find("textarea[name ='consist']").val($($("#contentTitle :selected")
+                    .val()).parent().find("[data-consist]").text());
+                var className = $($("#contentTitle :selected")
+                    .val()).parent().parent().find("[data-class]").attr("class");
+                if(className.indexOf('first') + 1) {
+                    $("#adminModal").find("input[name ='first']").prop("checked",true);
+                }
+                if(className.indexOf('second') + 1) {
+                    $("#adminModal").find("input[name ='second']").prop("checked",true);
+                }
+                if(className.indexOf('popular') + 1) {
+                    $("#adminModal").find("input[name ='popular']").prop("checked",true);
+                }
+                if(className.indexOf('new') + 1) {
+                    $("#adminModal").find("input[name ='new']").prop("checked",true);
+                }            
+            }
+
+            $('#changeContent').click( function(event){ // лoвим клик пo ссылке с клaссoм open_modal               
+                $("#contentTitle").attr("disabled",false);
+                changeSelect();
+            });
+
+            $('#dellContent').click( function(event){ // лoвим клик пo ссылке с клaссoм open_modal               
+                $("#contentTitle").attr("disabled",false);
+            });
+
+            $("#contentTitle").change(function() {
+                $("#adminModal").find("input[name ='first']").prop("checked",false);
+                $("#adminModal").find("input[name ='second']").prop("checked",false);
+                $("#adminModal").find("input[name ='popular']").prop("checked",false);
+                $("#adminModal").find("input[name ='new']").prop("checked",false);
+                changeSelect();
+            });
+
+            buttonDo.click( function(event){ // лoвим клик пo ссылке с клaссoм open_modal
+                if($('#addContent').hasClass('active')){
+                    addContent();
+                }
+                if($('#changeContent').hasClass('active')){
+                    changeContent();
+                }
+                if($('#dellContent').hasClass('active')){
+                    dellContent();
+                }
+            });
+
+            function dellContent(){
+                $.ajax({
+                    url: "/delpizza",
+                    method: "POST",
+                    data:  
+                    {
+                        pizza_id: $("#contentTitle :selected").val().substr(-1),
+                        URL: "./public/img/pizza/" + $($("#contentTitle :selected").val()).parent()
+                        .find('img')[1].src.split("/img/pizza/")[1],
+                    }
+                });   
+                $($("#contentTitle :selected").val()).parent().parent().remove();
+                 $('#adminModal').modal("hide");
+             }
+
+            function changeContent(){
+                var check = "all";
+                if ($(".adminContent").find("input[name='first']").prop('checked')){
+                    check = check + " " + "first";
+                }
+                if ($(".adminContent").find("input[name='second']").prop('checked')){
+                    check = check + " " + "second";
+                }   
+                if ($(".adminContent").find("input[name='popular']").prop('checked')){
+                    check = check + " " + "popular";
+                }
+                if ($(".adminContent").find("input[name='new']").prop('checked')){
+                    check = check + " " + "new";
+                }
+                $.ajax({
+                    url: "/pizzaChange",
+                    method: "POST",
+                    data:  
+                    {
+                        pizza_id: $("#contentTitle :selected").val().substr(-1),
+                        name:  $(".adminContent").find("input[name ='name']").val(),
+                        weight:  $(".adminContent").find("input[name ='weight']").val(),
+                        price: $(".adminContent").find("input[name ='price']").val(),
+                        consist:  $(".adminContent").find("textarea[name ='consist']").val(),
+                        categories: check,
+                    }
+                }).then(function(res) {
+                    alert("Данные записаны успешно!");
+                    $('#adminModal').modal("hide");
+                }); 
+            }
+
+            function addContent(){
+                $.ajax({
+                    url: "/uploadPizza",
+                    method: "POST",
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    data:  new FormData(jQuery('#uploadForm')[0])
+                }).then(function(res) {
+                    alert("Данные записаны успешно!");
+                    $('#adminModal').modal("hide");
+                });
+            }
         });
     }
 });
+
+
--- server.js
+++ server.js
@@ -2,13 +2,26 @@
 var app = express();
 var bodyParser = require("body-parser");
 var mysql      = require('mysql');
+var multer = require("multer");
+var fs = require("fs");
+var namefile;
 var connection = mysql.createConnection({
   	host     : 'localhost',
  	user     : 'root',
   	password : 'root',
   	database : 'vafood'
 });
-
+var storageOfPizza = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './public/img/Pizza/')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+        namefile = "img/pizza/" + file.originalname;
+    }
+});
+var uploadPizza = multer({ storage: storageOfPizza });
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.listen(3000);
@@ -60,11 +73,54 @@ app.post('/login', function (req, res) {
 	});
 });
 
-
+/// pizza
 app.get('/pizza', function (req, res, next) {
   connection.query('SELECT *  FROM pizza;', 
   function (error, results, fields) {
      if (error) throw error;
 	   res.send(results);
   });
+});
+
+app.post('/delpizza', function (req, res) {
+  fs.unlink(req.body.URL, function (err) {
+    if (err) throw err;
+    console.log("file deleted");
+  });
+  connection.query("DELETE FROM `pizza` WHERE `pizza_id`=" + req.body.pizza_id + ";", 
+  function (error, results, fields) {
+    if (error) throw error;
+  });
+});
+
+app.post('/uploadPizza', uploadPizza.single('fileupload-input'), function(req, res, next) {
+  var categories = "all ";
+  if(req.body.first == "on"){
+    categories = categories + " " + "first";
+  }
+  if (req.body.second == "on"){
+    categories = categories + " " + "second";
+  }
+  if (req.body.popular == "on"){
+    categories = categories + " " + "popular";
+  }   
+  if (req.body.new == "on"){
+    categories = categories + " " + "new";
+  }
+  connection.query("INSERT INTO pizza VALUES(NULL, '" + req.body.name + "', '" + req.body.weight + "', '" + 
+                   req.body.price + "', '" + namefile + "', '" + req.body.consist + "', '" + categories + "');", 
+  function (error, results, fields) {
+    if (error) throw error;
+  });
+  res.send('ok')
+});
+
+app.post('/pizzaChange', function (req, res) {
+  connection.query("UPDATE pizza  SET  name = '" + req.body.name + "', weight = '" + req.body.weight + "', price = '" + 
+                   req.body.price + "', consist = '" + req.body.consist +"', categories = '" + req.body.categories + 
+                   "' WHERE pizza_id = " + req.body.pizza_id +";", 
+  function (error, results, fields) {
+    if (error) throw error;
+  });
+   res.send('ok')
 });
